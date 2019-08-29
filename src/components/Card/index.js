813721ccb836b1f1ef1cf490af1557d830efa044
rafaelferreira_deway <rafael.ferreira@deway.com.br>
2019-08-29 15:39:38 -0300

    estilo principal finalizado

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+	return (
+		<Container>
+			<header>
+				<Label color="#7159c1" />
+				</header>
+				<p>do full server migration</p>
+					<img src="https://avatars0.githubusercontent.com/u/52254349?s=460&v=4" alt=""/>
+
+			</Container>
+	);
+}
